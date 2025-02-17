1948069889a7f43fcadfd5d39f3fe40b00bd3d9c
AhmadPijar <88574380+AhmadPijar@users.noreply.github.com>
2025-02-18 01:33:00 +0700

    Delete my portofolio directory

--- my portofolio/script.js
+++
@@ -1,8 +0,0 @@
-const menuNav = document.querySelector(".hamburger");
-const navbar = document.querySelector(".menubar");
-
-const toggleNav = () => {
-  navbar.classList.toggle("active");
-  menuNav.classList.toggle("hamburger-active");
-};
-menuNav.addEventListener("click", () => toggleNav());
